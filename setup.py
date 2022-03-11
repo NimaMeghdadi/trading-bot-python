6d28e9c4b37ae54742d1c2db8096c660f06cf53e
Nima <meghdadi.nima@gmail.com>
2022-03-12 02:00:33 +0330

    binance

--- setup.py
+++ setup.py
@@ -2,9 +2,31 @@ import asyncio
 from kucoin.client import Client
 from kucoin.asyncio import KucoinSocketManager
 
+import websockets
+import asyncio
+import json
+import time
+import matplotlib.pyplot as plt
+
 api_key = '<api_key>'
 api_secret = '<api_secret>'
 api_passphrase = '<api_passphrase>'
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+fig.show()
+
+xdata = []
+ydata = []
+
+
+def update_graph():
+    ax.plot(xdata, ydata, color='g')
+    ax.legend([f"Last price: {ydata[-1]}$"])
+    print("update garph")
+
+    fig.canvas.draw()
+    plt.pause(0.1)
 
 
 async def main():
@@ -13,8 +35,20 @@ async def main():
     # callback function that receives messages from the socket
     async def handle_evt(msg):
         if msg['topic'] == '/market/ticker:ETH-USDT':
-            price :int = {msg["data"]["price"]}
-            print(price)
+            price :float = {msg["data"]["price"]}
+            pricetime :int = {msg["data"]["time"]}
+
+            event_time = time.localtime()
+            event_time = f"{event_time.tm_hour}:{event_time.tm_min}:{event_time.tm_sec}"
+
+            print(event_time, price)
+
+            xdata.append(event_time)
+            print("update garph")
+            ydata.append(int(float(price)))
+
+            update_graph()
+            
 
 
 
@@ -26,7 +60,9 @@ async def main():
     ksm_private = await KucoinSocketManager.create(loop, client, handle_evt, private=True)
 
     # ETH-USDT Market Ticker
+    
     await ksm.subscribe('/market/ticker:ETH-USDT')
+
 
     while True:
         print("sleeping to keep loop open")
@@ -37,3 +73,55 @@ if __name__ == "__main__":
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
+
+
+# import websockets
+# import asyncio
+# import json
+# import time
+# import matplotlib.pyplot as plt
+
+
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# fig.show()
+
+# xdata = []
+# ydata = []
+
+
+# def update_graph():
+#     ax.plot(xdata, ydata, color='g')
+#     ax.legend([f"Last price: {ydata[-1]}$"])
+
+#     fig.canvas.draw()
+#     plt.pause(0.1)
+
+
+# async def main():
+#     global loop
+#     url = "wss://stream.binance.com:9443/stream?streams=btcusdt@miniTicker"
+
+#     async with websockets.connect(url) as client:
+#         while True:
+#             data = json.loads(await client.recv())['data']
+
+#             event_time = time.localtime(data['E'] // 1000)
+#             event_time = f"{event_time.tm_hour}:{event_time.tm_min}:{event_time.tm_sec}"
+
+#             print(event_time, data['c'])
+
+#             xdata.append(event_time)
+#             ydata.append(int(float(data['c'])))
+
+#             update_graph()
+
+# if __name__ == "__main__":
+
+#     loop = asyncio.get_event_loop()
+#     loop.run_until_complete(main())
+
+
+
+
+
